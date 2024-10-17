7571c7d519f97e4de0c022505e5d8bf658a1e711
victormelo <167628455+victormelo18@users.noreply.github.com>
2024-10-17 15:08:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let n1 = 7.5;
+let n2 = 8.0;
+let n3 = 6.5;
+
+let p1 = 3;
+let p2 = 2;
+let p3 = 5;
+
+let soma = (n1 * p1) + (n2 * p2) + (n3 * p3);
+let somaPesos = p1 + p2 + p3;
+let media = soma / somaPesos;
+
+
+
